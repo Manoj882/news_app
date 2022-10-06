6eac726012733d5b80e684310a878144b6c43bb1
unknown <manojbk488@gmail.com>
2022-10-06 20:01:30 +0545

    added search, category list item, carousel_slider

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,17 +1,22 @@
-
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/constants/constant.dart';
-
 
 import 'package:provider/provider.dart';
 
 import '../provider/news_provider.dart';
 
-
-class HomePageScreen extends StatelessWidget {
+class HomePageScreen extends StatefulWidget {
   const HomePageScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomePageScreen> createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
+  TextEditingController searchController = TextEditingController();
+  List<String> categoryItem = ['Top News', 'India', 'World', 'Finance', 'Health'];
+  CarouselController buttonCarouselController = CarouselController();
   @override
   Widget build(BuildContext context) {
     context.read<NewsProvider>().fetchData;
@@ -21,27 +26,112 @@ class HomePageScreen extends StatelessWidget {
       ),
       body: RefreshIndicator(
         onRefresh: () async {},
-        child: Center(
-          child: Consumer<NewsProvider>(
-            builder: ((context, data, child) {
-              return data.map.isEmpty && !data.error
-                  ? const CircularProgressIndicator()
-                  : data.error
-                      ? Text(
-                          'Oops, something went wrong. ${data.errorMessage}',
-                          textAlign: TextAlign.center,
-                        )
-                      : ListView.builder(
-                          itemCount: data.map.length,
-                          itemBuilder: (context, index) {
-                            return NewsCard(
-                              map: data.map['articles'][index],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        if ((searchController.text).replaceAll(' ', '') == '') {
+                          print('Blank search');
+                        } else {}
+                      },
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: searchController,
+                        textInputAction: TextInputAction.search,
+                        onSubmitted: (value) {
+                          print(value);
+                        },
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Search',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 50,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categoryItem.length,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        print(categoryItem[index]);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        margin: EdgeInsets.symmetric(horizontal: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Center(
+                          child: Text(
+                            categoryItem[index],
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Consumer<NewsProvider>(
+                builder: ((context, data, child) {
+                  return data.map.isEmpty && !data.error
+                      ? const CircularProgressIndicator()
+                      : data.error
+                          ? Text(
+                              'Oops, something went wrong. ${data.errorMessage}',
+                              textAlign: TextAlign.center,
+                            )
+                          : CarouselSlider.builder(
+                            carouselController: buttonCarouselController,
+                            options: CarouselOptions(
+                              autoPlay: false,
+                              height: 400,
+                              enableInfiniteScroll: false,
+                              aspectRatio: 16/9,
+                              enlargeCenterPage: true,
+                              scrollDirection: Axis.horizontal,
+
+                            ),
+
+                              itemCount: data.map.length,
+                              itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) {
+                                return NewsCard(
+                                  map: data.map['articles'][itemIndex],
+                                );
+                              },
+                              
                             );
-                          },
-                          shrinkWrap: true,
-                          primary: false,
-                        );
-            }),
+                }),
+              ),
+            ],
           ),
         ),
       ),
@@ -79,9 +169,7 @@ class NewsCard extends StatelessWidget {
               '${map['description']}',
               textAlign: TextAlign.justify,
             ),
-            const SizedBox(
-              height: 10,
-            ),
+            
           ],
         ),
       ),
